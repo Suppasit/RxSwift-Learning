683a2d8b24954fcde608dd3e9e5590588381e8fe
Suppasit Beer <monitorman_sc@hotmail.com>
2026-07-15 15:08:43 +0700

    feat: add new starter project and .gitignore

---
+++ 03-subjects/final/RxPlayground/PlaygroundDependencies/Package.swift
@@ -0,0 +1,44 @@
+// swift-tools-version: 6.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "PlaygroundDependencies",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "PlaygroundDependencies",
+            targets: ["PlaygroundDependencies"]
+        ),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "PlaygroundDependencies"
+        ),
+        .testTarget(
+            name: "PlaygroundDependenciesTests",
+            dependencies: ["PlaygroundDependencies"]
+        ),
+    ],
+    swiftLanguageModes: [.v6]
+)
+
+package.dependencies = [
+    .package(url: "https://github.com//ReactiveX/RxSwift", from: "6.10.2")
+]
+package.targets = [
+    .target(name: "PlaygroundDependencies",
+        dependencies: [
+            .product(name: "RxSwift", package: "RxSwift")
+        ]
+    )
+]
+package.platforms = [
+    .iOS("12.0"),
+    .macOS("10.13"),
+    .tvOS("12.0"),
+    .watchOS("4.0")
+]
