7c54d55906656e2d437cd89235a0771d62a6c8b2
Suppasit Beer <monitorman_sc@hotmail.com>
2026-08-13 09:57:48 +0700

    feat: add chapter 7

---
+++ 07-transforming-operators/challenge/Challenge1-Starter/RxPlayground/RxPlayground.playground/Sources/Unwrap.swift
@@ -0,0 +1,21 @@
+//
+//  Unwrap.swift
+//  
+//
+//  Created by Suppasit Chuwatsawat on 13/8/2569 BE.
+//
+
+import Foundation
+import RxSwift
+
+extension ObservableType {
+    
+    /**
+     Takes a sequence of optional elements and returns a sequence of non-optional elements, filtering out any nil values.
+     - returns: An observable sequence of non-optional elements
+     */
+    
+    public func unwrap<T>() -> Observable<T> where Element == T? {
+        return self.filter { $0 != nil }.map { $0! }
+    }
+}
